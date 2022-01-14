228639a2f0415881993296112f9d4149b03e53b2
= <lucas.mercier@isep.fr>
2022-01-14 14:37:46 -0500

    Splitting detection / faceid

---
+++ lab1/face_detection.py
@@ -0,0 +1,33 @@
+import cv2
+from utils import add_rectangles
+
+
+class FaceDetector:
+    def __init__(self, cascade_root_path) -> None:
+        self.face_cascade = cv2.CascadeClassifier(f'{cascade_root_path}/face_cascade.xml')
+        self.eye_cascade = cv2.CascadeClassifier(f'{cascade_root_path}/eye_cascade.xml')
+        self.smile_cascade = cv2.CascadeClassifier(f'{cascade_root_path}/smile_cascade.xml')
+
+
+    def _detect_in_region(self, img, detector, start_x, start_y):
+        """Detect in specific region"""
+        detected = detector.detectMultiScale(img, 1.3, 5)
+        inside_pos = [(x + start_x, y + start_y, w, h) for (x, y, w, h) in detected]
+        return inside_pos
+
+
+    def face_detection(self, img):
+        # improvements: delete boxes that just appeared
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        faces = self.face_cascade.detectMultiScale(img, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            region = img[y: y + h, x: x + w]
+            gray_region = gray[y: y + h, x: x + w]
+            eyes = self._detect_in_region(gray_region, self.eye_cascade, x, y)
+            smile = self._detect_in_region(region, self.smile_cascade, x, y)
+            img = add_rectangles(img, eyes, (0, 255, 0))
+            img = add_rectangles(img, smile, (0, 0, 255))
+
+        return img
--- lab1/main.py
+++ lab1/main.py
@@ -1,53 +1,16 @@
 import cv2
+import argparse
 from webcam_reader import display_webcam
+from face_detection import FaceDetector
 
 
-face_cascade = cv2.CascadeClassifier('xml/face_cascade.xml')
-eye_cascade = cv2.CascadeClassifier('xml/eye_cascade.xml')
-smile_cascade = cv2.CascadeClassifier('xml/smile_cascade.xml')
 
-def main(): 
-    display_webcam(face_detection)
-
-
-def add_rectangles(img, rectangles, color=(255, 0, 0)):
-    for (x, y, w, h) in rectangles:
-        img = cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
-    return img
-    
-
-def detect_intra(img, detector, start_x, start_y):
-    detected = detector.detectMultiScale(img, 1.3, 5)
-    inside_pos = [(x + start_x, y + start_y, w, h) for (x, y, w, h) in detected]
-    return inside_pos
-
-
-def face_detection(img):
-    # improvements: delete boxes that just appeared
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    faces = face_cascade.detectMultiScale(img, 1.3, 5)
-
-    for (x, y, w, h) in faces:
-        img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        region = img[y: y + h, x: x + w]
-        gray_region = gray[y: y + h, x: x + w]
-        eyes = detect_intra(gray_region, eye_cascade, x, y)
-        smile = detect_intra(region, smile_cascade, x, y)
-        img = add_rectangles(img, eyes, (0, 255, 0))
-        img = add_rectangles(img, smile, (0, 0, 255))
-
-    return img
-
-
-def transform(img):
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img = cv2.GaussianBlur(img, (9,9), 0)
-    return img
-
-
-def canny(img):
-    return cv2.Canny(img, 100, 200)
+def main_face_detection():
+    detector = FaceDetector(cascade_root_path='lab1/xml')
+    display_webcam(detector.face_detection)
 
 
 if __name__ == '__main__':
-    main()
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('--action', default='detection', help='Detection only')
+    main_face_detection()
